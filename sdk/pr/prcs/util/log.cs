28caa2b9db4f3f2e028cfe72a66728f2f2887e16
Paul@RYLOGIC2 <Paul@RYLOGIC2>
2012-06-21 15:58:31 +1200

    Removed silly process id related stuff

--- sdk/pr/prcs/util/log.cs
+++ sdk/pr/prcs/util/log.cs
@@ -4,7 +4,6 @@
 //***************************************************
 #define PR_LOGGING
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 
@@ -14,57 +13,58 @@ namespace pr.util
 	{
 		public class LogFile
 		{
-			public string m_fname;
-			public LogFile(string fname) { m_fname = fname; }
+			/// <summary>The log filepath</summary>
+			public string Filepath;
+			
+			public LogFile(string fname, bool reset)
+			{
+				Filepath = fname;
+				if (Filepath == null) return;
+				using (File.Open(Filepath, reset ? FileMode.Create : FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite)) {} // ensure the file exists/is reset
+			}
+			public void WriteLog(string str)
+			{
+				if (Filepath == null) { Debug.Write(str); return; }
+				using (StreamWriter f = new StreamWriter(File.Open(Filepath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite)))
+					f.Write(str);
+			}
 		}
 		
 		/// <summary>Sync object</summary>
 		private static readonly object m_lock = new object();
 
-		/// <summary>A map from process id to filename</summary>
-		private static readonly Dictionary<int, LogFile> m_logs = new Dictionary<int,LogFile>();
-
-		/// <summary>Return the current process id</summary>
-		private static int Id { get { return Process.GetCurrentProcess().Id; } }
-		
-		/// <summary>Creates a tag based on the namespcase and type of the sender</summary>
-		private static string GetTag(object sender)
-		{
-			if (sender == null) return "static";
-			Type sender_type = sender.GetType();
-			return string.Format("{0}.{1}", sender_type.Namespace, sender_type.Name);
-		}
+		/// <summary>The log file representation</summary>
+		private static LogFile m_log = new LogFile(null, false);
 
 		/// <summary>Register a log file for the current process id. Pass null for filepath to log to the debug window</summary>
 		[Conditional("PR_LOGGING")] public static void Register(string filepath, bool reset)
 		{
 			lock (m_lock)
 			{
-				try
-				{
-					if (filepath == null)
-					{
-						m_logs.Add(Id, null);
-					}
-					else
-					{
-						using (File.Open(filepath, reset ? FileMode.Create : FileMode.OpenOrCreate)) {}
-						m_logs.Add(Id, new LogFile(filepath));
-					}
-					return;
-				}
-				catch {}
-				m_logs.Add(Id, null);
+				// Ensure the directory exists (todo: should probably go in an official logs folder)
+				string dir = Path.GetDirectoryName(filepath);
+				if (dir != null && !Directory.Exists(dir))
+					Directory.CreateDirectory(dir);
+				
+				if (m_log == null)
+					m_log = new LogFile(filepath, reset);
+				else
+					m_log.Filepath = filepath;
 			}
 		}
 		
 		/// <summary>Add a string to the log</summary>
-		[Conditional("PR_LOGGING")] private static void Write(string str)
+		private static void Write(string str)
 		{
-			LogFile lf; lock (m_lock) lf = m_logs[Id];
-			if (lf == null) { Debug.Write(str); return; }
-			using (StreamWriter f = new StreamWriter(File.Open(lf.m_fname, FileMode.Append, FileAccess.Write, FileShare.ReadWrite)))
-				f.Write(str);
+			lock (m_lock) m_log.WriteLog(str);
+		}
+
+		/// <summary>Creates a tag based on the namespcase and type of the sender</summary>
+		private static string GetTag(object sender)
+		{
+			if (sender == null) return "static";
+			Type sender_type = sender.GetType();
+			return string.Format("{0}.{1}", sender_type.Namespace, sender_type.Name);
 		}
 
 		/// <summary>Write info to the current log</summary>
