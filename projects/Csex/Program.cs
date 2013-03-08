33f3ac4bfd9166088ed6d1f82fbfc151a0e9e445
p_ryland <none@none>
2013-03-08 16:00:30 +1300

    Added a thing for finding conflicting assembly dependencies

--- projects/Csex/Program.cs
+++ projects/Csex/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
 using pr.common;
 using pr.crypt;
@@ -46,6 +49,8 @@ namespace Csex
 				"  Syntax: Csex -command [parameters]\n"+
 				"    -gencode   : Generates an activation code\n"+
 				"    -signfile  : Sign a file using RSA\n"+
+				"    -find_assembly_conflicts : Recursively checks assemblies\n" +
+				"      for version conflicts in their dependent assemblies" +
 				// NEW_COMMAND - add a help string
 				"\n"+
 				"  Type Cex -command -help for help on a particular command\n"+
@@ -61,6 +66,7 @@ namespace Csex
 				{
 				case "-gencode":  m_cmd = new GenActivationCode(); break;
 				case "-signfile": m_cmd = new SignFile(); break;
+				case "-find_assembly_conflicts": m_cmd = new FindAssemblyConflicts(); break;
 				// NEW_COMMAND - handle the command
 				}
 			}
@@ -190,6 +196,81 @@ namespace Csex
 			{
 				Console.WriteLine("Signing failed: "+ex.Message);
 			}
+		}
+	}
+	#endregion
+
+	#region Find Assembly Conflicts
+	public class FindAssemblyConflicts :Cmd
+	{
+		private class Reference
+		{
+			public AssemblyName Assembly           { get; set; }
+			public AssemblyName ReferencedAssembly { get; set; }
+		}
+		private string m_dir;
+
+		/// <summary>Display help information in the case of an invalid command line</summary>
+		public override void ShowHelp()
+		{
+			Console.Write(
+				"This command checks .NET assemblies for mismatches in dependent assembly versions\n" +
+				" Syntax: Csex -find_assembly_conflicts [-p directory]\n" +
+				"  -p : the directory to check. If not given the current directory is checked\n" +
+				"");
+		}
+
+		/// <summary>Handle a command line option.</summary>
+		public override bool CmdLineOption(string option, string[] args, ref int arg)
+		{
+			switch (option)
+			{
+			default: return base.CmdLineOption(option, args, ref arg);
+			case "-find_assembly_conflicts": return true;
+			case "-p": m_dir = args[arg++]; return true;
+			}
+		}
+
+		/// <summary>Run the command</summary>
+		public override void Run()
+		{
+			if (string.IsNullOrEmpty(m_dir))
+				m_dir = Environment.CurrentDirectory;
+			
+			var assemblies = GetAllAssemblies(m_dir);
+			var references = GetReferencesFromAllAssemblies(assemblies);
+			var groupsOfConflicts = FindReferencesWithTheSameShortNameButDiffererntFullNames(references);
+			foreach (var group in groupsOfConflicts)
+			{
+				Console.Out.WriteLine("Possible conflicts for {0}:", group.Key);
+				foreach (var reference in group)
+					Console.Out.WriteLine("{0} references {1}" ,reference.Assembly.Name.PadRight(25) ,reference.ReferencedAssembly.FullName);
+			}
+		}
+		private IEnumerable<IGrouping<string, Reference>> FindReferencesWithTheSameShortNameButDiffererntFullNames(IEnumerable<Reference> references)
+		{
+			return
+				from reference in references
+				group reference by reference.ReferencedAssembly.Name
+				into referenceGroup
+				where referenceGroup.ToList().Select(reference => reference.ReferencedAssembly.FullName).Distinct().Count() > 1
+				select referenceGroup;
+		}
+		private IEnumerable<Reference> GetReferencesFromAllAssemblies(IEnumerable<Assembly> assemblies)
+		{
+			var references = new List<Reference>();
+			foreach (var assembly in assemblies)
+			foreach (var referencedAssembly in assembly.GetReferencedAssemblies())
+				references.Add(new Reference{Assembly = assembly.GetName(),ReferencedAssembly = referencedAssembly});
+			return references;
+		}
+		private IEnumerable<Assembly> GetAllAssemblies(string path)
+		{
+			var files = new List<FileInfo>();
+			var directoryToSearch = new DirectoryInfo(path);
+			files.AddRange(directoryToSearch.GetFiles("*.dll", SearchOption.AllDirectories));
+			files.AddRange(directoryToSearch.GetFiles("*.exe", SearchOption.AllDirectories));
+			return files.ConvertAll(file => Assembly.LoadFile(file.FullName));
 		}
 	}
 	#endregion
