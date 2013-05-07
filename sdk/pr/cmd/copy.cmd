2886e79b302b455c0f581925656b4e9d91df56bb
p_ryland <none@none>
2013-05-07 17:09:51 +1200

    removed fc from copy.cmd as it's too slow

--- sdk/pr/cmd/copy.cmd
+++ sdk/pr/cmd/copy.cmd
@@ -5,18 +5,9 @@
 :: Note: if 'dst' is in a directory, it needs to exist before calling copy
 :copy
 	setlocal
-	set src=%~1
-	set dst=%~2
-	set changed=1
-	if exist "%dst%" (
-		fc /B "%src%" "%dst%" >nul
-		if errorlevel 0 set changed=0
-	) else (
-		echo 0 > "%dst%"
-	)
-	if [%changed%]==[1] (
-		echo "%src%" -^> "%dst%"
-		xcopy "%src%" "%dst%" /Y /F >nul
-	)
+	set src=%~f1
+	set dst=%~f2
+	echo %src% -^> %dst%
+	xcopy "%src%" "%dst%" /Y /F /D >nul
 	endlocal
 goto :eof
