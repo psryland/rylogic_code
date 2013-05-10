487aac780cc1c574cb0ee3e7ed3ec72928d42120
p_ryland <none@none>
2013-05-11 11:22:24 +1200

    added template batch file

---
+++ batch_template.cmd
@@ -0,0 +1,42 @@
+::Template file for making batch files
+::Use:
+::  file.cmd $(TargetPath) $(PlatformTarget) $(ConfigurationName) [dstsubdir]
+
+@echo off
+SetLocal EnableDelayedExpansion 
+
+::Assign variables from inputs
+set targetpath=%~1
+set platform=%2
+set config=%3
+set dstsubdir=%~4
+if [%platform%]==[win32] set platform=x86
+
+::Load Rylogic environment variables and check version
+if [%RylogicEnv%]==[] (
+	echo ERROR: The 'RylogicEnv' environment variable is not set.
+	goto :eof
+)
+call %RylogicEnv%
+if %RylogicEnvVersion% lss 1 (
+	echo ERROR: '%RylogicEnv%' is out of date. Please update.
+	goto :eof
+)
+
+:: Add the sdk\pr\cmd folder to the path
+set PATH=%qdrive%\sdk\pr\cmd\;%PATH%
+
+echo This is a template batchfile
+
+echo.
+echo Success.
+echo.
+call wait 5000
+goto :eof
+
+:error
+echo.
+echo Failed.
+echo.
+pause
+goto :eof
