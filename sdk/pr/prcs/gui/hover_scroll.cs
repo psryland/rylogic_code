232d4f55799e70a3dd09a2afeea660ea705c7790
p_ryland <none@none>
2013-06-14 01:32:35 +1200

    Created ZoomScroll control

--- sdk/pr/prcs/gui/hover_scroll.cs
+++ sdk/pr/prcs/gui/hover_scroll.cs
@@ -20,10 +20,12 @@ namespace pr.gui
 		[DllImport("user32.dll")] private static extern IntPtr WindowFromPoint(Point pt);
 		[DllImport("user32.dll")] private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wp, IntPtr lp);
 
-		private readonly List<IntPtr> m_wnds = null;
+		/// <summary>The window handles of the controls that should detect hoverscrolling</summary>
+		public List<IntPtr> WindowHandles { get; private set; }
 
-		public HoverScroll() {}
-		public HoverScroll(params IntPtr[] wnds) { m_wnds = new List<IntPtr>(wnds); }
+		public HoverScroll()                     { WindowHandles = new List<IntPtr>(); }
+		public HoverScroll(params IntPtr[] wnds) { WindowHandles = new List<IntPtr>(wnds); }
+
 		public bool PreFilterMessage(ref Message m)
 		{
 			const int WM_MOUSEWHEEL = 0x20a;
@@ -33,10 +35,10 @@ namespace pr.gui
 			Point pos = new Point(m.LParam.ToInt32() & 0xffff, m.LParam.ToInt32() >> 16);
 			IntPtr hWnd = WindowFromPoint(pos);
 
-			if (hWnd == IntPtr.Zero) return false;						// No window...
-			if (hWnd == m.HWnd)	return false;							// Hovering over the correct control already
-			if (Control.FromHandle(hWnd) == null) return false;			// Not over a control
-			if (m_wnds != null && !m_wnds.Contains(hWnd)) return false;	// Not a control we're hoverscrolling
+			if (hWnd == IntPtr.Zero) return false;              // No window...
+			if (hWnd == m.HWnd)	return false;                   // Hovering over the correct control already
+			if (Control.FromHandle(hWnd) == null) return false; // Not over a control
+			if (WindowHandles.Count != 0 && !WindowHandles.Contains(hWnd)) return false; // Not a control we're hoverscrolling
 
 			SendMessage(hWnd, m.Msg, m.WParam, m.LParam);
 			return true;
