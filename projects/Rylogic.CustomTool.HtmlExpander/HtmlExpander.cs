81c12206d82a0040bd03020b274487c65c82bc55
p_ryland <none@none>
2013-09-03 01:58:34 +1200

    CustomTool for expanding html templates in VS

---
+++ projects/Rylogic.CustomTool.HtmlExpander/HtmlExpander.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TextTemplating.VSHost;
+using pr.common;
+using pr.extn;
+using pr.script;
+
+// http://mnaoumov.wordpress.com/2012/09/26/developing-custom-tool-aka-single-file-generators-for-visual-studio-2012/
+
+namespace Rylogic.CustomTool
+{
+	[ComVisible(true)]
+	[Guid("a72434a4-0a4d-4cc2-aba2-42e1fcd61db4")]
+	public class HtmlExpander :BaseCodeGeneratorWithSite
+	{
+		/// <summary>Gets the default extension for this generator</summary>
+		public override string GetDefaultExtension()
+		{
+			return ".htm";
+		}
+
+		/// <summary>Generate the code-behind</summary>
+		protected override byte[] GenerateCode(string src_filepath, string src)
+		{
+			try
+			{
+				var current_dir = Path.GetDirectoryName(src_filepath) ?? string.Empty;
+				var result = Expand.Html(new StringSrc(src), current_dir);
+				return Encoding.UTF8.GetBytes(result);
+			}
+			catch (Exception ex)
+			{
+				var msg = "HTML Expansion failed" + Environment.NewLine + "Exception:" + Environment.NewLine + ex.MessageFull();
+				return Encoding.UTF8.GetBytes(msg);
+			}
+		}
+	}
+}
