2be6219b27b305c2ad8dc92e636e09c26aa89bcc
p_ryland <none@none>
2013-09-03 01:53:57 +1200

    dmd build script works on head directory

--- dlang/build_all.cmd
+++ dlang/build_all.cmd
@@ -15,25 +15,27 @@ if %RylogicEnvVersion% lss 5 (
 	goto :eof
 )
 
+set dmdhead=%dmdroot%\..\head\dmd2
+
 echo *************************************************************************
 echo  Building DMD and related tools
-echo    RootDir: %dmdroot%
+echo    RootDir: %dmdhead%
 echo *************************************************************************
 pause
 
 pushd
-set mk=%dmdroot%\windows\bin\make.exe
+set mk=%dmdhead%\windows\bin\make.exe
 
 echo ------------------------------------------------------------------------
 echo. Building dmd.exe
 echo ------------------------------------------------------------------------
 set VisualStudioVersion=11.0
-"%msbuild%" "%dmdroot%\src\dmd\src\dmd_msc.vcxproj" /t:Build /p:Configuration=Release /p:Platform=x64
+"%msbuild%" "%dmdhead%\src\dmd\src\dmd_msc.vcxproj" /t:Build /p:Configuration=Release /p:Platform=x64
 if errorlevel 1 goto :error
-xcopy "%dmdroot%\src\dmd\src\vcbuild\x64\Release\dmd_msc.exe" "%dmdroot%\windows\bin\dmd.exe" /Y /F
+xcopy "%dmdhead%\src\dmd\src\vcbuild\x64\Release\dmd_msc.exe" "%dmdhead%\windows\bin\dmd.exe" /Y /F
 echo.
-::cd "%dmdroot%\src\dmd\src"
-::set DM_HOME=%dmdroot%
+::cd "%dmdhead%\src\dmd\src"
+::set DM_HOME=%dmdhead%
 ::call "%vc_env%"
 ::"%mk%" -f win32.mak CC=vcbuild\dmc_cl INCLUDE=vcbuild DEBUG=/O2 dmd.exe
 ::if errorlevel 1 goto :error
@@ -42,7 +44,7 @@ echo.
 echo ------------------------------------------------------------------------
 echo Making druntime
 echo ------------------------------------------------------------------------
-cd "%dmdroot%\src\druntime"
+cd "%dmdhead%\src\druntime"
 "%mk%" -f win64.mak clean
 "%mk%" -f win64.mak
 if errorlevel 1 goto :error
@@ -51,11 +53,11 @@ echo.
 echo ------------------------------------------------------------------------
 echo Making phobos
 echo ------------------------------------------------------------------------
-cd "%dmdroot%\src\phobos"
+cd "%dmdhead%\src\phobos"
 "%mk%" -f win64.mak clean
 "%mk%" -f win64.mak phobos64.lib
 if errorlevel 1 goto :error
-xcopy phobos64.lib "%dmdroot%\windows\lib\phobos64.lib" /Y /F
+xcopy phobos64.lib "%dmdhead%\windows\lib\phobos64.lib" /Y /F
 echo.
 
 goto :success
