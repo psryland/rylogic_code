583e4107af5ce063bd01badd1987d87e11bf6f0a
p_ryland <none@none>
2013-09-25 01:59:00 +1200

    More text substitutions for click actions

--- projects/RyLogViewer/src/settings/clkaction.cs
+++ projects/RyLogViewer/src/settings/clkaction.cs
@@ -1,31 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Xml.Linq;
 
 namespace RyLogViewer
 {
 	public static class SpecialTags
 	{
-		public const string FileName = "{[FileName]}";
-		public const string FileDir  = "{[FileDir]}";
+		public const string FilePath  = "FilePath";
+		public const string FileName  = "FileName";
+		public const string FileTitle = "FileTitle";
+		public const string FileDir   = "FileDir";
+		public const string FileExtn  = "FileExtn";
+		public const string FileRoot  = "FileRoot";
 	}
 
-	/// <summary>An action that occurs in responce to a click on a row that matches a pattern</summary>
+	/// <summary>An action that occurs in response to a click on a row that matches a pattern</summary>
 	public class ClkAction :Pattern
 	{
 		/// <summary>The program to launch when activated</summary>
 		public string Executable { get; set; }
-		
+
 		/// <summary>Arguments passed to the program to launch</summary>
 		public string Arguments { get; set; }
-		
+
 		/// <summary>The working directory of the launched program</summary>
 		public string WorkingDirectory { get; set; }
-		
-		/// <summary>Return a string description of the acton</summary>
+
+		/// <summary>Return a string description of the action</summary>
 		public string ActionString { get { return Executable + " " + Arguments; } }
-		
+
 		public ClkAction()
 		{
 			Executable       = "";
@@ -38,8 +46,6 @@ namespace RyLogViewer
 			Arguments        = rhs.Arguments;
 			WorkingDirectory = rhs.WorkingDirectory;
 		}
-		
-		/// <summary>Construct from xml description</summary>
 		public ClkAction(XElement node) :base(node)
 		{
 			// ReSharper disable PossibleNullReferenceException
@@ -49,28 +55,77 @@ namespace RyLogViewer
 			// ReSharper restore PossibleNullReferenceException
 		}
 
-		/// <summary>Performs the click action using 'text'. 'filepath' should be the full filepath containing the line 'text'</summary>
+		/// <summary>The expanded command line</summary>
+		public string CommandLine(string text, string filepath)
+		{
+			return Expand(Executable, text, filepath) + " " + Expand(Arguments, text, filepath);
+		}
+
+		/// <summary>Performs the click action using 'matched_text'. 'filepath' should be the full filepath containing the line 'matched_text'</summary>
 		public void Execute(string text, string filepath)
 		{
-			// Substitute the capture groups into the arguments string
-			var grps = CaptureGroups(text);
-			string args = Arguments;
-			foreach (var c in grps)
-				args = args.Replace("{"+c.Key+"}", c.Value.Trim());
-
-			// Perform special tag substitutions
-			args = args.Replace(SpecialTags.FileName, Path.GetFileName(filepath) ?? string.Empty);
-			args = args.Replace(SpecialTags.FileDir , Path.GetDirectoryName(filepath) ?? string.Empty);
-
 			// Create the process
 			ProcessStartInfo info = new ProcessStartInfo
 			{
 				UseShellExecute        = false,
-				FileName               = Executable,
-				Arguments              = args,
-				WorkingDirectory       = WorkingDirectory
+				FileName               = Expand(Executable, text, filepath),
+				Arguments              = Expand(Arguments, text, filepath),
+				WorkingDirectory       = Expand(WorkingDirectory, text, filepath),
 			};
 			Process.Start(info);
+		}
+
+		/// <summary>Returns the arguments expanded with the substitutions</summary>
+		private string Expand(string str, string matched_text, string filepath)
+		{
+			// Environment variable substitutions
+			var substs = new Dictionary<string, string>();
+			foreach (var v in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>())
+				substs.Add(v.Key.ToString(), v.Value.ToString());
+
+			// Special tag substitutions
+			substs.Add(SpecialTags.FilePath  , filepath ?? string.Empty);
+			substs.Add(SpecialTags.FileName  , Path.GetFileName(filepath) ?? string.Empty);
+			substs.Add(SpecialTags.FileTitle , Path.GetFileNameWithoutExtension(filepath) ?? string.Empty);
+			substs.Add(SpecialTags.FileDir   , Path.GetDirectoryName(filepath) ?? string.Empty);
+			substs.Add(SpecialTags.FileExtn  , Path.GetExtension(filepath) ?? string.Empty);
+			substs.Add(SpecialTags.FileRoot  , Path.GetPathRoot(filepath) ?? string.Empty);
+
+			// Capture groups substitutions
+			var grps = CaptureGroups(matched_text);
+			foreach (var c in grps)
+				substs.Add(c.Key, c.Value.Trim());
+
+			// Perform substitutions
+			var sb = new StringBuilder(str.Length);
+			for (int i = 0, iend = str.Length; i != iend;)
+			{
+				if (str[i] == '{')
+				{
+					if (++i == iend)    { sb.Append('{'); break; }
+					if (str[i] == '{') { sb.Append('{'); ++i; continue; }
+
+					int j;
+					for (j = i; j != iend && str[j] != '}'; ++j) {}
+					if (j == iend) { sb.Append(str.Substring(i)); break; }
+
+					var key = str.Substring(i, j - i);
+					string value;
+					sb.Append(substs.TryGetValue(key, out value) ? value : key);
+					i = j + 1;
+					continue;
+				}
+				if (str[i] == '}')
+				{
+					if (++i == iend)    { sb.Append('}'); break; }
+					if (str[i] == '}') { sb.Append('}'); ++i; continue; }
+					continue;
+				}
+				sb.Append(str[i]);
+				++i;
+			}
+
+			return sb.ToString();
 		}
 
 		/// <summary>Export this ClkAction as xml</summary>
@@ -85,33 +140,33 @@ namespace RyLogViewer
 			);
 			return node;
 		}
-		
+
 		/// <summary>Reads an xml description of the ClkAction expressions</summary>
 		public static List<ClkAction> Import(string actions)
 		{
 			var list = new List<ClkAction>();
-			
+
 			XDocument doc;
 			try { doc = XDocument.Parse(actions); } catch { return list; }
 			if (doc.Root == null) return list;
 			foreach (XElement n in doc.Root.Elements(XmlTag.ClkAction))
 				try { list.Add(new ClkAction(n)); } catch {} // Ignore those that fail
-			
+
 			return list;
 		}
-		
+
 		/// <summary>Serialise the ClkAction patterns to xml</summary>
 		public static string Export(IEnumerable<ClkAction> ClkActions)
 		{
 			XDocument doc = new XDocument(new XElement(XmlTag.Root));
 			if (doc.Root == null) return "";
-			
+
 			foreach (var ac in ClkActions)
 				doc.Root.Add(ac.ToXml(new XElement(XmlTag.ClkAction)));
-			
+
 			return doc.ToString(SaveOptions.None);
 		}
-		
+
 		/// <summary>Creates a new object that is a copy of the current instance.</summary>
 		public override object Clone()
 		{
--- projects/RyLogViewer/src/settings/clkaction_ui.cs
+++ projects/RyLogViewer/src/settings/clkaction_ui.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 using RyLogViewer.Properties;
 using pr.extn;
-using pr.util;
 
 namespace RyLogViewer
 {
@@ -18,50 +20,68 @@ namespace RyLogViewer
 		private Button m_btn_ok;
 		private Button m_btn_cancel;
 		private Button m_btn_capture_groups;
+		private Label m_lbl_preview;
+		private TextBox m_text_preview;
 		private Button m_btn_browse;
-		
+
 		/// <summary>The action configured by this UI</summary>
 		public ClkAction Action { get; private set; }
-		
+
 		public ClkActionUI(ClkAction action)
 		{
 			InitializeComponent();
 			m_tt = new ToolTip();
 			Action = new ClkAction(action);
+			FocusedEditbox = m_edit_exec;
 			string tt;
-			
+
 			// Executable
 			tt = "The program or batch file to execute";
 			m_lbl_exec.ToolTip(m_tt, tt);
 			m_edit_exec.ToolTip(m_tt, tt);
 			m_edit_exec.Text = Action.Executable;
+			m_edit_exec.GotFocus += (s,a) =>
+				{
+					FocusedEditbox = m_edit_exec;
+				};
 			m_edit_exec.TextChanged += (s,a)=>
 				{
 					Action.Executable = m_edit_exec.Text;
+					UpdateUI();
 				};
-			
+
 			// Arguments
 			tt = "The arguments to pass to the executable.\r\n" +
-			     "For sub string or wildcard patterns use {1} to represent the matched text.\r\n" +
-			     "For regular expressions use {1},{2},{3},... to represent capture groups";
+			     "For sub string or wildcard patterns use {0} to represent the matched text.\r\n" +
+			     "For regular expressions use {0},{1},{2},... to represent capture groups";
 			m_lbl_args.ToolTip(m_tt, tt);
 			m_edit_args.ToolTip(m_tt, tt);
 			m_edit_args.Text = Action.Arguments;
+			m_edit_args.GotFocus += (s,a) =>
+				{
+					FocusedEditbox = m_edit_args;
+				};
 			m_edit_args.TextChanged += (s,a)=>
 				{
 					Action.Arguments = m_edit_args.Text;
+					UpdateUI();
 				};
-			
+
 			// Start in directory
 			tt = "The working directory to start the executable in";
 			m_lbl_startin.ToolTip(m_tt, tt);
 			m_edit_startin.ToolTip(m_tt,tt);
 			m_edit_startin.Text = Action.WorkingDirectory;
+			m_edit_startin.GotFocus += (s,a) =>
+				{
+					FocusedEditbox = m_edit_startin;
+				};
 			m_edit_startin.TextChanged += (s,a)=>
 				{
 					Action.WorkingDirectory = m_edit_startin.Text;
+					UpdateUI();
 				};
-			
+
 			// Browse button
 			m_btn_browse.ToolTip(m_tt, "Browse for an executable to run");
 			m_btn_browse.Click += (s,a)=>
@@ -69,32 +89,109 @@ namespace RyLogViewer
 					var dg = new OpenFileDialog{Title = "Select an Executable", CheckPathExists = true, Filter = Resources.ExecutablesFilter};
 					if (dg.ShowDialog(this) != DialogResult.OK) return;
 					m_edit_exec.Text = dg.FileName;
+					UpdateUI();
 				};
 
 			// Capture groups button
 			m_btn_capture_groups.ToolTip(m_tt, "Displays a menu of the pattern match tags and special tags");
 			m_btn_capture_groups.Click += (s,a) =>
 				{
-					// Pop up a menu with the special tags
-					var menu = new ContextMenuStrip();
-					foreach (var x in Action.CaptureGroupNames)
-					{
-						var group_name = x;
-						menu.Items.Add("Capture Group: {0}".Fmt(group_name), null, (ss,aa) => m_edit_args.SelectedText = "{"+group_name+"}");
-					}
-					if (menu.Items.Count != 0)
-					{
-						menu.Items.Add(new ToolStripSeparator());
-					}
-					menu.Items.Add("Current file name"     , null, (ss,aa) => m_edit_args.SelectedText = SpecialTags.FileName);
-					menu.Items.Add("Current file directory", null, (ss,aa) => m_edit_args.SelectedText = SpecialTags.FileDir);
-					menu.Show(m_btn_capture_groups, m_btn_capture_groups.Width, 0);
+					ShowCaptureGroupsMenu();
+					UpdateUI();
 				};
 
 			Disposed += (s,a) =>
 				{
 					m_tt.Dispose();
 				};
+
+			UpdateUI();
+		}
+
+		/// <summary>Returns the edit box that currently has focus (or null)</summary>
+		private TextBox FocusedEditbox { get; set; }
+
+		/// <summary>Display the capture groups menu</summary>
+		private void ShowCaptureGroupsMenu()
+		{
+			var edit = FocusedEditbox;
+			if (edit == null)
+				return;
+
+			// Pop up a menu with the special tags
+			var menu = new ContextMenuStrip();
+			foreach (var x in Action.CaptureGroupNames)
+			{
+				var group_name = x;
+				menu.Items.Add("Capture Group: {0}".Fmt(group_name), null, (ss,aa) => edit.SelectedText = "{"+group_name+"}");
+			}
+			if (menu.Items.Count != 0) menu.Items.Add(new ToolStripSeparator());
+			menu.Items.Add("Current file full path" , null, (ss,aa) => edit.SelectedText = "{" + SpecialTags.FilePath  + "}");
+			menu.Items.Add("Current file name"      , null, (ss,aa) => edit.SelectedText = "{" + SpecialTags.FileName  + "}");
+			menu.Items.Add("Current file directory" , null, (ss,aa) => edit.SelectedText = "{" + SpecialTags.FileDir   + "}");
+			menu.Items.Add("Current file title"     , null, (ss,aa) => edit.SelectedText = "{" + SpecialTags.FileTitle + "}");
+			menu.Items.Add("Current file extension" , null, (ss,aa) => edit.SelectedText = "{" + SpecialTags.FileExtn  + "}");
+			menu.Items.Add("Current file drive"     , null, (ss,aa) => edit.SelectedText = "{" + SpecialTags.FileRoot  + "}");
+			menu.Items.Add(new ToolStripSeparator());
+			menu.Items.Add("Environment Variable...", null, (ss,aa) => ShowEnvironmentVariables(this, (k,v) => edit.SelectedText = "{"+k+"}"));
+			menu.Show(m_btn_capture_groups, m_btn_capture_groups.Width, 0);
+		}
+
+		/// <summary>Display a dialog containing the environment variables</summary>
+		private void ShowEnvironmentVariables(IWin32Window owner, Action<string,string> on_selected)
+		{
+			var form = new Form
+				{
+					FormBorderStyle = FormBorderStyle.SizableToolWindow,
+					StartPosition = FormStartPosition.CenterParent,
+					Size = new Size(620,300)
+				};
+
+			var list = new ListView
+				{
+					View          = View.Details,
+					Dock          = DockStyle.Fill,
+					FullRowSelect = true,
+					Scrollable    = true,
+					MultiSelect   = false,
+				};
+
+			list.Columns.Add("Key", -2, HorizontalAlignment.Left);
+			list.Columns.Add("Value", -2, HorizontalAlignment.Left);
+			foreach (var env_var in Environment.GetEnvironmentVariables().Cast<DictionaryEntry>())
+			{
+				var key = env_var.Key.ToString();
+				var val = env_var.Value.ToString();
+				list.Items.Add(new ListViewItem(new []{key, val}));
+			}
+
+			Action accept = () =>
+				{
+					var selected = list.SelectedItems;
+					if (selected.Count == 0) return;
+					var items = selected[0].SubItems;
+					on_selected(items[0].Text, items[1].Text);
+					form.Close();
+				};
+
+			list.MouseDoubleClick += (s,a) =>
+				{
+					accept();
+				};
+			list.KeyDown += (s,a) =>
+				{
+					if (a.KeyCode == Keys.Enter)
+						accept();
+				};
+
+			form.Controls.Add(list);
+			form.ShowDialog(owner);
+		}
+
+		/// <summary>Update the UI</summary>
+		private void UpdateUI(object sender = null, EventArgs args = null)
+		{
+			m_text_preview.Text = Action.CommandLine(string.Empty, @"x:\directory\filename.extn");
 		}
 
 		#region Windows Form Designer generated code
@@ -134,10 +231,12 @@ namespace RyLogViewer
 			this.m_btn_cancel = new System.Windows.Forms.Button();
 			this.m_btn_browse = new System.Windows.Forms.Button();
 			this.m_btn_capture_groups = new System.Windows.Forms.Button();
+			this.m_lbl_preview = new System.Windows.Forms.Label();
+			this.m_text_preview = new System.Windows.Forms.TextBox();
 			this.SuspendLayout();
-			// 
+			//
 			// m_lbl_exec
-			// 
+			//
 			this.m_lbl_exec.AutoSize = true;
 			this.m_lbl_exec.Location = new System.Drawing.Point(16, 11);
 			this.m_lbl_exec.Name = "m_lbl_exec";
@@ -145,9 +244,9 @@ namespace RyLogViewer
 			this.m_lbl_exec.TabIndex = 0;
 			this.m_lbl_exec.Text = "Execute:";
 			this.m_lbl_exec.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
+			//
 			// m_lbl_args
-			// 
+			//
 			this.m_lbl_args.AutoSize = true;
 			this.m_lbl_args.Location = new System.Drawing.Point(5, 35);
 			this.m_lbl_args.Name = "m_lbl_args";
@@ -155,9 +254,9 @@ namespace RyLogViewer
 			this.m_lbl_args.TabIndex = 1;
 			this.m_lbl_args.Text = "Arguments:";
 			this.m_lbl_args.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
+			//
 			// m_lbl_startin
-			// 
+			//
 			this.m_lbl_startin.AutoSize = true;
 			this.m_lbl_startin.Location = new System.Drawing.Point(22, 61);
 			this.m_lbl_startin.Name = "m_lbl_startin";
@@ -165,82 +264,107 @@ namespace RyLogViewer
 			this.m_lbl_startin.TabIndex = 2;
 			this.m_lbl_startin.Text = "Start in:";
 			this.m_lbl_startin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-			// 
+			//
 			// m_edit_exec
-			// 
-			this.m_edit_exec.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			//
+			this.m_edit_exec.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_edit_exec.Location = new System.Drawing.Point(71, 8);
 			this.m_edit_exec.Name = "m_edit_exec";
-			this.m_edit_exec.Size = new System.Drawing.Size(185, 20);
-			this.m_edit_exec.TabIndex = 0;
-			// 
+			this.m_edit_exec.Size = new System.Drawing.Size(315, 20);
+			this.m_edit_exec.TabIndex = 2;
+			//
 			// m_edit_args
-			// 
-			this.m_edit_args.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			//
+			this.m_edit_args.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_edit_args.Location = new System.Drawing.Point(71, 33);
 			this.m_edit_args.Name = "m_edit_args";
-			this.m_edit_args.Size = new System.Drawing.Size(185, 20);
-			this.m_edit_args.TabIndex = 1;
-			// 
+			this.m_edit_args.Size = new System.Drawing.Size(315, 20);
+			this.m_edit_args.TabIndex = 3;
+			//
 			// m_edit_startin
-			// 
-			this.m_edit_startin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			//
+			this.m_edit_startin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_edit_startin.Location = new System.Drawing.Point(71, 58);
 			this.m_edit_startin.Name = "m_edit_startin";
-			this.m_edit_startin.Size = new System.Drawing.Size(185, 20);
-			this.m_edit_startin.TabIndex = 2;
-			// 
+			this.m_edit_startin.Size = new System.Drawing.Size(315, 20);
+			this.m_edit_startin.TabIndex = 4;
+			//
 			// m_btn_ok
-			// 
+			//
 			this.m_btn_ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_btn_ok.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.m_btn_ok.Location = new System.Drawing.Point(139, 84);
+			this.m_btn_ok.Location = new System.Drawing.Point(269, 124);
 			this.m_btn_ok.Name = "m_btn_ok";
 			this.m_btn_ok.Size = new System.Drawing.Size(75, 23);
-			this.m_btn_ok.TabIndex = 4;
+			this.m_btn_ok.TabIndex = 5;
 			this.m_btn_ok.Text = "OK";
 			this.m_btn_ok.UseVisualStyleBackColor = true;
-			// 
+			//
 			// m_btn_cancel
-			// 
+			//
 			this.m_btn_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.m_btn_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.m_btn_cancel.Location = new System.Drawing.Point(220, 84);
+			this.m_btn_cancel.Location = new System.Drawing.Point(350, 124);
 			this.m_btn_cancel.Name = "m_btn_cancel";
 			this.m_btn_cancel.Size = new System.Drawing.Size(75, 23);
-			this.m_btn_cancel.TabIndex = 3;
+			this.m_btn_cancel.TabIndex = 6;
 			this.m_btn_cancel.Text = "Cancel";
 			this.m_btn_cancel.UseVisualStyleBackColor = true;
-			// 
+			//
 			// m_btn_browse
-			// 
+			//
 			this.m_btn_browse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.m_btn_browse.Location = new System.Drawing.Point(262, 6);
+			this.m_btn_browse.Location = new System.Drawing.Point(392, 6);
 			this.m_btn_browse.Name = "m_btn_browse";
 			this.m_btn_browse.Size = new System.Drawing.Size(33, 23);
-			this.m_btn_browse.TabIndex = 5;
+			this.m_btn_browse.TabIndex = 0;
 			this.m_btn_browse.Text = "...";
 			this.m_btn_browse.UseVisualStyleBackColor = true;
-			// 
+			//
 			// m_btn_capture_groups
-			// 
-			this.m_btn_capture_groups.Location = new System.Drawing.Point(262, 30);
+			//
+			this.m_btn_capture_groups.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.m_btn_capture_groups.Location = new System.Drawing.Point(392, 30);
 			this.m_btn_capture_groups.Name = "m_btn_capture_groups";
 			this.m_btn_capture_groups.Size = new System.Drawing.Size(33, 24);
-			this.m_btn_capture_groups.TabIndex = 6;
+			this.m_btn_capture_groups.TabIndex = 1;
 			this.m_btn_capture_groups.Text = ">";
 			this.m_btn_capture_groups.UseVisualStyleBackColor = true;
-			// 
+			//
+			// m_lbl_preview
+			//
+			this.m_lbl_preview.AutoSize = true;
+			this.m_lbl_preview.Location = new System.Drawing.Point(16, 86);
+			this.m_lbl_preview.Name = "m_lbl_preview";
+			this.m_lbl_preview.Size = new System.Drawing.Size(48, 13);
+			this.m_lbl_preview.TabIndex = 7;
+			this.m_lbl_preview.Text = "Preview:";
+			this.m_lbl_preview.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			//
+			// m_text_preview
+			//
+			this.m_text_preview.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.m_text_preview.Location = new System.Drawing.Point(71, 83);
+			this.m_text_preview.Multiline = true;
+			this.m_text_preview.Name = "m_text_preview";
+			this.m_text_preview.ReadOnly = true;
+			this.m_text_preview.Size = new System.Drawing.Size(315, 35);
+			this.m_text_preview.TabIndex = 8;
+			this.m_text_preview.TabStop = false;
+			//
 			// ClkActionUI
-			// 
+			//
 			this.AcceptButton = this.m_btn_ok;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.CancelButton = this.m_btn_cancel;
-			this.ClientSize = new System.Drawing.Size(307, 116);
+			this.ClientSize = new System.Drawing.Size(437, 156);
+			this.Controls.Add(this.m_text_preview);
+			this.Controls.Add(this.m_lbl_preview);
 			this.Controls.Add(this.m_btn_capture_groups);
 			this.Controls.Add(this.m_btn_browse);
 			this.Controls.Add(this.m_btn_cancel);
@@ -253,14 +377,13 @@ namespace RyLogViewer
 			this.Controls.Add(this.m_lbl_exec);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximumSize = new System.Drawing.Size(3000, 150);
-			this.MinimumSize = new System.Drawing.Size(200, 150);
+			this.MaximumSize = new System.Drawing.Size(3000, 190);
+			this.MinimumSize = new System.Drawing.Size(200, 190);
 			this.Name = "ClkActionUI";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
 			this.Text = "Configure Click Action";
 			this.ResumeLayout(false);
 			this.PerformLayout();
-
 		}
 
 		#endregion
