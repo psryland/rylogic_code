daec5fd2a0a49d2ac397251d22adb18ef1740b4c
p_ryland <none@none>
2014-08-07 13:32:24 +1200

    Bug fix for BitStr templatised high/low bit / index functions

--- sdk/pr/pr/maths/bitfunctions.h
+++ sdk/pr/pr/maths/bitfunctions.h
@@ -74,6 +74,20 @@ namespace pr
 
 	// Returns the bit position of the highest bit
 	// Also, is the floor of the log base 2 for a 32 bit integer
+	template <typename T> inline T HighBitIndex(T n)
+	{
+		typedef std::make_unsigned<T>::type UT;
+
+		T pos = 0;
+		int bits = 4 * sizeof(T);
+		for (UT mask = UT(~0) >> bits; bits; bits >>= 1, mask >>= bits)
+		{
+			auto shift = ((n & ~mask) != 0) * bits;
+			n >>= shift;
+			pos |= shift;
+		}
+		return pos;
+	}
 	inline uint HighBitIndex(uint n)
 	{
 		unsigned int shift, pos = 0;
@@ -86,16 +100,16 @@ namespace pr
 	}
 
 	// Returns the bit position of the lowest bit
-	inline uint LowBitIndex(uint n)
+	template <typename T> inline T LowBitIndex(T n)
 	{
 		return HighBitIndex(LowBit(n));
 	}
 
 	// Return a bit mask contain only the highest bit of 'n'
 	// Must be a faster way?
-	inline uint HitBit(uint n)
+	template <typename T> inline T HighBit(T n)
 	{
-		return 1 << HighBitIndex(n);
+		return T(1) << HighBitIndex(n);
 	}
 
 	// Returns true if 'n' is a exact power of two
@@ -158,16 +172,23 @@ namespace pr
 	template <typename T> inline char const* BitStr(T bits, bool leading_zeros = false)
 	{
 		thread_local static char str[sizeof(T) * 8 + 1];
+		typedef std::make_unsigned<T>::type UT;
+		UT mask = UT(1) << (sizeof(T) * 8 - 1);
+		
+		if (!leading_zeros) // skip leading zeros
+			for (; (mask & bits) == 0; mask >>= 1) {}
 
-		T const mask = T(1) << (sizeof(T) * 8 - 1);
-		if (!leading_zeros)
-			for (; bits != 0 && (mask & bits) == 0; bits <<= 1) {} // skip leading zeros
-
-		int i;
-		for (i = 0; bits != 0; ++i, bits <<= 1)
-			str[i] = (mask & bits) != 0 ? '1' : '0';
-		str[i] = 0;
-
+		char* ptr = str;
+		if (mask == 0)
+		{
+			*ptr++ = '0';
+		}
+		else
+		{
+			for (; mask != 0; mask >>= 1)
+				*ptr++ = (mask & bits) != 0 ? '1' : '0';
+		}
+		*ptr = 0;
 		return str;
 	}
 
@@ -211,21 +232,50 @@ namespace pr
 	{
 		PRUnitTest(pr_maths_bitfunctions)
 		{
-			char const* mask_str = "1001010011";
-			auto mask = Bits<long long>(mask_str);
+			{
+				char const* mask_str = "1001010011";
+				auto mask = Bits<long long>(mask_str);
+				PR_CHECK(BitStr(mask), mask_str);
 
-			PR_CHECK(BitStr(mask), mask_str);
+				std::vector<long long> bits;
+				for (auto b : pr::EnumerateBits(mask))
+					bits.push_back(b);
 
-			std::vector<long long> bits;
-			for (auto b : pr::EnumerateBits(mask))
-				bits.push_back(b);
-
-			PR_CHECK(bits.size(), 5U);
-			PR_CHECK(bits[0], 1U << 0);
-			PR_CHECK(bits[1], 1U << 1);
-			PR_CHECK(bits[2], 1U << 4);
-			PR_CHECK(bits[3], 1U << 6);
-			PR_CHECK(bits[4], 1U << 9);
+				PR_CHECK(bits.size(), 5U);
+				PR_CHECK(bits[0], 1U << 0);
+				PR_CHECK(bits[1], 1U << 1);
+				PR_CHECK(bits[2], 1U << 4);
+				PR_CHECK(bits[3], 1U << 6);
+				PR_CHECK(bits[4], 1U << 9);
+			}
+			{
+				char const* mask_str = "1001110010";
+				auto mask = Bits<uint>(mask_str);
+				PR_CHECK(mask, 626U);
+				PR_CHECK(BitStr(mask), mask_str);
+				PR_CHECK(HighBitIndex(mask), 9U);
+				PR_CHECK(LowBitIndex(mask), 1U);
+				PR_CHECK(LowBit(mask), 2U);
+				PR_CHECK(HighBit(mask), 0x200U);
+			}
+			{
+				char const* mask_str = "1111010100010";
+				auto mask = Bits<short>(mask_str);
+				PR_CHECK(mask, 7842);
+				PR_CHECK(HighBitIndex(mask), 12);
+				PR_CHECK(LowBitIndex(mask), 1);
+				PR_CHECK(LowBit(mask), 2);
+				PR_CHECK(HighBit(mask), 0x1000);
+			}
+			{
+				char const* mask_str = "1001001100110010101010010100111010010110010101110110000110100100";
+				auto mask = Bits<unsigned long long>(mask_str);
+				PR_CHECK(mask, 0x9332A94E965761A4ULL);
+				PR_CHECK(HighBitIndex(mask), 63);
+				PR_CHECK(LowBitIndex(mask), 2U);
+				PR_CHECK(LowBit(mask), 4);
+				PR_CHECK(HighBit(mask), 0x8000000000000000ULL);
+			}
 		}
 	}
 }
