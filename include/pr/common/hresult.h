a9181e4806a1c4b7bba22e64a428f6747b3a12e6
p_ryland <none@none>
2014-09-05 12:36:32 +1200

    fix for unicode

--- include/pr/common/hresult.h
+++ include/pr/common/hresult.h
@@ -92,9 +92,9 @@ namespace pr
 		#endif
 
 		// else ask windows
-		TCHAR msg[16384];
-		DWORD length(sizeof(msg) / sizeof(TCHAR));
-		if (FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS, NULL, result, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msg, length, NULL))
+		char msg[16384];
+		DWORD length(sizeof(msg));
+		if (FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM|FORMAT_MESSAGE_IGNORE_INSERTS, NULL, result, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), msg, length, NULL))
 		{
 			return msg;
 		}
