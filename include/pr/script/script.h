a5cc6154218ccef2077918d2f7b334df709b6ad6
Paul Ryland <psryland@gmail.com>
2015-07-29 21:51:44 +1200

    added script wrapper header

---
+++ include/pr/script/script.h
@@ -0,0 +1,17 @@
+//**********************************
+// Script
+//  Copyright (c) Rylogic Ltd 2015
+//**********************************
+
+#pragma once
+
+#include "pr/script/forward.h"
+#include "pr/script/fail_policy.h"
+#include "pr/script/location.h"
+#include "pr/script/script_core.h"
+#include "pr/script/filter.h"
+#include "pr/script/macros.h"
+#include "pr/script/includes.h"
+#include "pr/script/embedded.h"
+#include "pr/script/preprocessor.h"
+#include "pr/script/tokeniser.h"
