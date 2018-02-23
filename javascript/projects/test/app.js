393d4cea0c654621cb2b696bcd128087a356e508
psryland <none@none>
2018-02-23 15:34:56 +1300

    Removed javascript code (in favour of typescript)

--- javascript/projects/test/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var pr = require('../../lib/rylogic')
-
-let m = pr.m4x4.create();
-console.log(pr.m4x4.GetX(m));
--- javascript/rylogic/deploy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*- 
-
-import shutil, subprocess
-
-subprocess.check_call(["npm", "run", "build-all"], cwd=".\\", shell=True)
-shutil.copy2(".\\dist\\rylogic.js", "..\\lib\\")
-shutil.copy2(".\\dist\\rylogic.min.js", "..\\lib\\")
