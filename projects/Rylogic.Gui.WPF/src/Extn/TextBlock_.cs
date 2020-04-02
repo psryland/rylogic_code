c91b8b7871e8417c00c08901409890c987ba3d2a
Paul Ryland <psryland@yahoo.co.nz>
2020-04-02 14:47:26 +1300

    DataGrid column resizing

--- projects/Rylogic.Gui.WPF/src/Extn/TextBlock_.cs
+++ projects/Rylogic.Gui.WPF/src/Extn/TextBlock_.cs
@@ -5,11 +5,11 @@ using System.Windows.Media;
 
 namespace Rylogic.Gui.WPF
 {
-	public class TextBlock_
+	public static class TextBlock_
 	{
 		/// <summary>Foreground Attached property</summary>
 		public const int Foreground = 0;
-		public static readonly DependencyProperty ForegroundProperty = Gui_.DPRegisterAttached<TextBlock_>(nameof(Foreground));
+		public static readonly DependencyProperty ForegroundProperty = Gui_.DPRegisterAttached(typeof(TextBlock_), nameof(Foreground));
 		public static SolidColorBrush GetForeground(DependencyObject obj) => (SolidColorBrush)obj.GetValue(ForegroundProperty);
 		public static void SetForeground(DependencyObject obj, SolidColorBrush value) => obj.SetValue(ForegroundProperty, value);
 		private static void Foreground_Changed(DependencyObject obj)
