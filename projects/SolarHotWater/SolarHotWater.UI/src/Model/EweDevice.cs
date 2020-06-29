41f7ab635164bff312c0116be5f066177d741c3d
Paul Ryland <psryland@yahoo.co.nz>
2020-06-30 00:29:32 +1200

    WIP on SolarHotWater thingy

---
+++ projects/SolarHotWater/SolarHotWater.UI/src/Model/EweDevice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarHotWater
+{
+	public class EweDevice
+	{
+		/// <summary>Device name</summary>
+		public string Name => name;
+
+		public string name = string.Empty;
+	}
+}
---
+++ projects/SolarHotWater/SolarHotWater.UI/src/Model/Settings.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Rylogic.Common;
+
+namespace SolarHotWater
+{
+	public class SettingsData :SettingsBase<SettingsData>
+	{
+		public SettingsData()
+		{
+			Username = string.Empty;
+			Password = string.Empty;
+			SolarInverterIP = "ShierlawJ.RyLAN";
+			SolarPollRate = TimeSpan.FromSeconds(10);
+			AutoSaveOnChanges = true;
+		}
+		public SettingsData(string filepath)
+			: base(filepath, ESettingsLoadFlags.None)
+		{
+			AutoSaveOnChanges = true;
+		}
+
+		/// <summary>EweLink Login Email</summary>
+		public string Username
+		{
+			get => get<string>(nameof(Username));
+			set => set(nameof(Username), value);
+		}
+
+		/// <summary>Plain text password...</summary>
+		public string Password
+		{
+			get => get<string>(nameof(Password));
+			set => set(nameof(Password), value);
+		}
+
+		/// <summary>The IP address of the Solar Inverter</summary>
+		public string SolarInverterIP
+		{
+			get => get<string>(nameof(SolarInverterIP));
+			set => set(nameof(SolarInverterIP), value);
+		}
+
+		/// <summary>How fast to poll the solar inverter</summary>
+		public TimeSpan SolarPollRate
+		{
+			get => get<TimeSpan>(nameof(SolarPollRate));
+			set => set(nameof(SolarPollRate), value);
+		}
+	}
+}
