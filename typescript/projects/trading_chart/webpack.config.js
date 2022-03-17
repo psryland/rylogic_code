145408ca0885daf34b82331313a574c49e83ab1d
Paul Ryland <psryland@gmail.com>
2022-03-18 09:37:29 +1300

    Fixes for typescript vulnerabilities

--- typescript/projects/trading_chart/webpack.config.js
+++ typescript/projects/trading_chart/webpack.config.js
@@ -1,29 +1,29 @@
 var path = require('path');
 
 module.exports =
-	{
-		mode: "production",
-		entry: {
-			trading_chart: "./src/trading_chart.ts",
-		},
-		output: {
-			filename: "./dist/[name].bundle.js",
-			sourceMapFilename: "./dist/[name].bundle.js.map",
-			libraryTarget: "umd",
-			pathinfo: true
-		},
-		resolve: {
-			extensions: [".ts"]
-		},
-		module: {
-			rules: [{
-				use:[{
-					loader: "ts-loader",
-					options: {
-						transpileOnly: false
-					}
-				}]
+{
+	mode: "production",
+	entry: {
+		trading_chart: "./src/trading_chart.ts",
+	},
+	output: {
+		filename: "./dist/[name].bundle.js",
+		sourceMapFilename: "./dist/[name].bundle.js.map",
+		libraryTarget: "umd",
+		pathinfo: true
+	},
+	resolve: {
+		extensions: [".ts"]
+	},
+	module: {
+		rules: [{
+			use:[{
+				loader: "ts-loader",
+				options: {
+					transpileOnly: false
+				}
 			}]
-		},
-		plugins: []
-	}
+		}]
+	},
+	plugins: []
+}
