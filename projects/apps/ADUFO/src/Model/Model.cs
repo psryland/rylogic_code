0c616ed7ac4d60e43b89749f8b7b8fdd84c4a5a9
Paul Ryland <psryland@gmail.com>
2023-04-24 13:28:10 +1200

    ADUFO initial work

---
+++ projects/apps/ADUFO/src/Model/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADUFO;
+
+public class Model :IDisposable
+{
+	public Model(Settings settings)
+	{
+		Settings = settings;
+	}
+	public void Dispose()
+	{
+	}
+
+	/// <summary>Application settings</summary>
+	private Settings Settings { get; }
+
+	/// <summary>Access to ADO</summary>
+	public AdoInterface Ado { get; set; } = null!;
+
+	public void ConnectToADO()
+	{
+		// Connect to Azure DevOps
+
+		// Download user stories from Azure DevOps
+
+	}
+}
---
+++ projects/apps/ADUFO/src/UI/ConnectUI.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using Rylogic.Gui.WPF;
+
+namespace ADUFO;
+
+public partial class ConnectUI : Window, INotifyPropertyChanged
+{
+	public ConnectUI(Window owner)
+	{
+		Owner = owner;
+
+		Accept = Command.Create(this, AcceptInternal);
+
+		InitializeComponent();
+		DataContext = this;
+	}
+
+	/// <summary>The ADO organisation</summary>
+	public string Organization
+	{
+		get => m_organization;
+		set
+		{
+			if (m_organization == value) return;
+			m_organization = value;
+			NotifyPropertyChanged(nameof(Organization));
+			NotifyPropertyChanged(nameof(AdoUrl));
+		}
+	}
+	private string m_organization = string.Empty;
+
+	/// <summary>The access token for ADO</summary>
+	public string PersonalAccessToken
+	{
+		get => m_personal_access_token;
+		set
+		{
+			if (m_personal_access_token == value) return;
+			m_personal_access_token = value;
+			NotifyPropertyChanged(nameof(PersonalAccessToken));
+		}
+	}
+	private string m_personal_access_token = string.Empty;
+
+	/// <summary>The ADO URL</summary>
+	public string AdoUrl => $"https://dev.azure.com/{Organization}";
+
+	/// <summary>Accept action</summary>
+	public Command Accept { get; }
+	private void AcceptInternal()
+	{
+		DialogResult = true;
+		Close();
+	}
+
+	/// <inheritdoc/>
+	public event PropertyChangedEventHandler? PropertyChanged;
+	private void NotifyPropertyChanged(string prop_name) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop_name));
+}
