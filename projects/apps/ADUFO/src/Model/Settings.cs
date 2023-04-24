2f11ce68002fde7a7f6265a34b21fc0cc4ca95b4
Paul Ryland <psryland@gmail.com>
2023-04-24 13:28:10 +1200

    ADUFO initial work

---
+++ projects/apps/ADUFO/src/Model/Settings.cs
@@ -0,0 +1,32 @@
+﻿using Rylogic.Common;
+
+namespace ADUFO;
+
+public class Settings : SettingsBase<Settings>
+{
+	public Settings()
+	{
+		Organization = string.Empty;
+		PersonalAccessToken = string.Empty;
+		AutoSaveOnChanges = true;
+	}
+	public Settings(string filepath, ESettingsLoadFlags flags = ESettingsLoadFlags.None)
+		: base(filepath, flags)
+	{
+		AutoSaveOnChanges = true;
+	}
+
+	/// <summary>The ADO organization</summary>
+	public string Organization
+	{
+		get => get<string>(nameof(Organization));
+		set => set(nameof(Organization), value);
+	}
+
+	/// <summary>The Personal Access Token used to access ADO</summary>
+	public string PersonalAccessToken
+	{
+		get => get<string>(nameof(PersonalAccessToken));
+		set => set(nameof(PersonalAccessToken), value);
+	}
+}
---
+++ projects/apps/ADUFO/src/UI/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.VisualStudio.Services.Organization.Client;
+using Rylogic.Gui.WPF;
+
+namespace ADUFO;
+
+public partial class MainWindow : Window
+{
+	public MainWindow(Settings settings, Model model)
+	{
+		Settings = settings;
+		Model = model;
+
+		InitializeComponent();
+		Loaded += (o,s) =>
+		{
+			if (Settings.Organization.Length == 0 || Settings.PersonalAccessToken.Length == 0)
+				PromptConnectionSettings();
+		};
+	}
+
+	/// <summary>Application settings</summary>
+	private Settings Settings { get; }
+
+	/// <summary>Application logic</summary>
+	private Model Model { get; }
+
+	/// <summary>Update the connection settings</summary>
+	private void PromptConnectionSettings()
+	{
+		// Prompt for the ADO connection and PAT if not in the settings
+		var dlg = new ConnectUI(this)
+		{
+			Organization = Settings.Organization,
+			PersonalAccessToken = Settings.PersonalAccessToken,
+		};
+		if (dlg.ShowDialog() != true)
+		{
+			// Terminate the application
+			Close();
+			return;
+		}
+		Settings.Organization = dlg.Organization;
+		Settings.PersonalAccessToken = dlg.PersonalAccessToken;
+	}
+
+}
