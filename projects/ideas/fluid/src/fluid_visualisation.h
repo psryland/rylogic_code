77e30c4f2f52b68c56b0adff3ee2f0e292d552a1
Paul Ryland <paulryland@microsoft.com>
2024-01-13 10:52:19 +1300

    Fluid sim/ Field WIP

--- projects/ideas/fluid/src/fluid_visualisation.h
+++ projects/ideas/fluid/src/fluid_visualisation.h
@@ -1,6 +1,7 @@
 // Fluid
 #pragma once
 #include "src/forward.h"
+#include "src/probe.h"
 
 namespace pr::fluid
 {
@@ -10,16 +11,26 @@ namespace pr::fluid
 			x(rdr12::ModelPtr, m_model, rdr12::EInstComp::ModelPtr)
 		PR_RDR12_DEFINE_INSTANCE(Instance, PR_INST)
 		#undef PR_INST
+		using PointShaderPtr = rdr12::RefPtr<rdr12::shaders::PointSpriteGS>;
 
-		FluidSimulation const* m_sim;
+		FluidSimulation* m_sim;
 		rdr12::Renderer* m_rdr;
-		rdr12::ShaderPtr m_gs_points;
-		Instance m_container;
-		Instance m_fluid;
+		rdr12::Scene* m_scn;
+		PointShaderPtr m_gs_points;
+		Instance m_gfx_container;
+		Instance m_gfx_fluid;
+		Instance m_gfx_gradient;
+		Probe m_probe;
 
-		FluidVisualisation(FluidSimulation const& sim, rdr12::Renderer& rdr);
+		FluidVisualisation(FluidSimulation& sim, rdr12::Renderer& rdr, rdr12::Scene& scn);
 		~FluidVisualisation();
 
 		void AddToScene(rdr12::Scene& scene);
+
+		// Handle input
+		void OnMouseButton(gui::MouseEventArgs& args);
+		void OnMouseMove(gui::MouseEventArgs& args);
+		void OnMouseWheel(gui::MouseWheelArgs& args);
+		void OnKey(gui::KeyEventArgs& args);
 	};
 }
--- projects/ideas/fluid/src/spatial_partition.cpp
+++ projects/ideas/fluid/src/spatial_partition.cpp
@@ -1,6 +1,6 @@
 // Fluid
 #include "src/spatial_partition.h"
-#include "src/particles.h"
+#include "src/particle.h"
 
 namespace pr::fluid
 {
@@ -13,23 +13,36 @@ namespace pr::fluid
 	{
 		m_pivots.resize(particles.size());
 
-		kdtree::Build<3, float, Particle>(
+		kdtree::Build<Dimensions, float, Particle, kdtree::EStrategy::AxisByLevel>(
 			particles,
 			[](Particle const& p, int a) { return p.m_pos[a]; },
 			[&](Particle& p, int a) { m_pivots[&p - particles.data()] = s_cast<int8_t>(a); }
 		);
 	}
 
-	void SpatialPartition::Find(std::span<Particle const> particles, v4_cref position, float radius, std::function<void(Particle const&)> found) const
+	void SpatialPartition::Find(v4_cref position, float radius, std::span<Particle const> particles, std::function<void(Particle const&, float)> found) const
 	{
-		float const search[3] = { position.x,position.y,position.z };
-
-		kdtree::Find<3, float, Particle>(
-			particles,
-			search, radius,
-			[](Particle const& p, int a) { return p.m_pos[a]; },
-			[&](Particle const& p) { return s_cast<int>(m_pivots[&p - particles.data()]); },
-			[&](Particle const& p, float) { found(p); }
-		);
+		if constexpr (Dimensions == 2)
+		{
+			float const search[2] = { position.x, position.y };
+			kdtree::Find<2, float, Particle>(
+				particles,
+				search, radius,
+				[](Particle const& p, int a) { return p.m_pos[a]; },
+				[&](Particle const& p) { return s_cast<int>(m_pivots[&p - particles.data()]); },
+				[&](Particle const& p, float dist_sq) { found(p, dist_sq); }
+			);
+		}
+		if constexpr (Dimensions == 3)
+		{
+			float const search[3] = { position.x, position.y, position.z };
+			kdtree::Find<3, float, Particle>(
+				particles,
+				search, radius,
+				[](Particle const& p, int a) { return p.m_pos[a]; },
+				[&](Particle const& p) { return s_cast<int>(m_pivots[&p - particles.data()]); },
+				[&](Particle const& p, float dist_sq) { found(p, dist_sq); }
+			);
+		}
 	}
 }
