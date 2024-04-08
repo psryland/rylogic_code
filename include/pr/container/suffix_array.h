414fa47da70dc571d09c2e3ab6b8ceb0dbecba83
Paul Ryland <psryland@gmail.com>
2024-04-08 23:54:28 +1200

    More refactoring of suffix array implementation

--- include/pr/container/suffix_array.h
+++ include/pr/container/suffix_array.h
@@ -27,48 +27,117 @@ namespace pr::suffix_array
 
 	namespace impl
 	{
-		// Find the start or end of each bucket
-		template <std::integral Int>
-		void GetBuckets(std::span<Int const> data, std::span<int> bkt, bool end)
+		struct SuffixType
 		{
+			std::vector<bool> m_stypes;
+
+			template <std::integral Int>
+			SuffixType(std::span<Int const> data)
+				:m_stypes(data.size())
+			{
+				// Classify the type of each character into S or L types. Suffixes that aren't S-type are L-type.
+				// The sentinel must be in 'data', important!!!
+				m_stypes[std::ssize(data) - 1] = true; // The expected sentinal position
+				m_stypes[std::ssize(data) - 2] = false; // One before the sentinal
+				for (auto i = std::ssize(data) - 3; i >= 0; --i)
+				{
+					m_stypes[i] =
+						(data[i] <  data[i + 1]) ||
+						(data[i] == data[i + 1] && m_stypes[i + 1]);
+				}
+			}
+
+
+			bool IsSType(int i) const
+			{
+				return i >= 0 && m_stypes[i];
+			}
+			bool IsLType(int i) const
+			{
+				return i >= 0 && !m_stypes[i];
+			}
+
+			// True if index 'i' is an "LMS" (i.e. leftmost S-type) character
+			bool IsLeftmostSType(int i) const
+			{
+				return i > 0 && IsSType(i) && IsLType(i - 1);
+			}
+		};
+
+		// Returns, in 'bkt', the index position of the start or end of the character buckets.
+		template <std::integral Int, bool StartIndices>
+		void GetBuckets(std::span<Int const> data, std::span<int> bkt)
+		{
+			// Buckets are ranges within the SA that contain all the suffixes that start with a particular character.
+			// e.g. {$|aaaaaa|bbbb|ccc|dddd}
+
 			// Compute the size of each bucket
 			std::fill(bkt.begin(), bkt.end(), 0);
 			for (auto d : data)
 				bkt[d]++;
 
-			// Convert the counts into an accumulation
-			int sum = 0;
-			for (auto& b : bkt)
+			// Return the indices in 'bkt'
+			if constexpr (StartIndices)
 			{
-				sum += b;
-				b = end ? sum : sum - b;
+				// Find the index for the start of each character bucket
+				int sum = 0;
+				for (auto& b : bkt)
+				{
+					sum += b;
+					b = sum - b;
+				}
+			}
+			else
+			{
+				// Find the index for the end of each character bucket
+				int sum = 0;
+				for (auto& b : bkt)
+				{
+					sum += b;
+					b = sum;
+				}
 			}
 		}
 
-		// Compute Suffix Array L
+		// Sort L-type suffix indices by induction from left to right
 		template <std::integral Int>
-		void InduceSuffixArrayL(std::vector<bool> const& s_types, std::span<int> SA, std::span<Int const> data, std::span<int> bkt, bool end)
+		void InduceSortLtoR(SuffixType const& sfx_type, std::span<int> SA, std::span<Int const> data, std::span<int> bkt)
 		{
 			// Find the starts of the buckets
-			GetBuckets<Int>(data, bkt, end);
+			GetBuckets<Int, true>(data, bkt);
+
+			// Left to Right pass
 			for (auto i = 0; i != std::ssize(data); ++i)
 			{
+				// Look at each 'known' index in 'SA'
+				if (SA[i] == -1)
+					continue;
+
+				// Read the type of the immediately prior character. If the type is 'L-type' then write
+				// its index into the associated bucket in 'SA', filling from the left.
+				// The 'SA' is assumed to contain '-1' for unknown indices.
 				auto j = SA[i] - 1;
-				if (j >= 0 && !s_types[j])
+				if (j >= 0 && sfx_type.IsLType(j))
 					SA[bkt[data[j]]++] = j;
 			}
 		}
 
-		// Compute Suffix Array S
+		// Sort R-type suffix indices by induction from right to left
 		template <std::integral Int>
-		void InduceSuffixArrayS(std::vector<bool> const& s_types, std::span<int> SA, std::span<Int const> data, std::span<int> bkt, bool end)
+		void InduceSortRtoL(SuffixType const& sfx_type, std::span<int> SA, std::span<Int const> data, std::span<int> bkt)
 		{
 			// Find the ends of the buckets
-			GetBuckets<Int>(data, bkt, end);
+			GetBuckets<Int, false>(data, bkt);
+
+			// Right to Left pass
 			for (auto i = std::ssize(data) - 1; i >= 0; --i)
 			{
+				// Look at each 'known' index in 'SA'
+				if (SA[i] == -1)
+					continue;
+
 				auto j = SA[i] - 1;
-				if (j >= 0 && s_types[j])
+				if (j >= 0 && sfx_type.IsSType(j))
 					SA[--bkt[data[j]]] = j;
 			}
 		}
@@ -79,8 +148,6 @@ namespace pr::suffix_array
 	template <std::integral Int>
 	void Build(std::span<Int const> data, std::span<int> SA, int alphabet_size)
 	{
-		// Uses a working space (excluding 'data' and 'SA') of at most 2.25n+O(1) for a constant alphabet.
-		//
 		// Basic notation commonly used in the presentations of this algorithm.
 		// Let S be a string of n characters stored in an array[0..n − 1], and Σ(S) be the alphabet of S.
 		// For a substring, S[i]S[i + 1]...S[j] in S, we denote it as S[i..j].
@@ -101,7 +168,19 @@ namespace pr::suffix_array
 		// So:
 		//  S-type means "this suffix is smaller than the next adjacent suffix".
 		//  L-type means "this suffix is larger than the next adjacent suffix".
-
+		//
+		// Uses a working space (excluding 'data' and 'SA') of at most 2.25n+O(1) for a constant alphabet.
+		//
+		// OK, WTF does this do?
+		// 1. use a bit array to mark the relationship between adjacent suffixes as either S or L.
+		// 2. Find the 'LMS' suffixes. These partition the suffixes into blocks of guaranteed increasing order.
+		// 3. The R-to-L pass adds all LMS indices to the ends of their buckets. These are not yet sorted.
+		// 4. The L-to-R pass adds L-type suffixes.
+		//     The first L-type suffix is correctly sorted w.r.t. to the LMS suffixes already in SA.
+		//     Each subsequent L-type suffix is then correctly sorted w.r.t. to the previous L-type suffixes,
+		//     because if it wasn't, there would have been a contradiction in the S/L classification.
+		// 5. The next R-to-L pass adds the S-type suffixes correctly sorted.
+		
 		using namespace impl;
 
 		// Handle degenerate cases
@@ -123,122 +202,115 @@ namespace pr::suffix_array
 			return;
 		}
 
-		// L/S-type array in bits. Suffixes that aren't S-type are L-type.
-		auto s_types = std::vector<bool>(std::size(data));
-		static auto IsLeftmostSType = [](int i, std::vector<bool> const& s_types) -> bool
-		{
-			return i > 0 && s_types[i] && !s_types[i - 1];
-		};
+		// Classify the type of each character into S or L types
+		SuffixType sfx_type(data);
 
-		// Classify the type of each character into S or L types.
-		// The sentinel must be in 'data', important!!!
-		s_types[std::ssize(data) - 2] = 0; // One before the sentinal
-		s_types[std::ssize(data) - 1] = 1; // The expected sentinal position
-		for (auto i = std::ssize(data) - 3; i >= 0; --i)
-		{
-			s_types[i] =
-				(data[i] <  data[i + 1]) ||
-				(data[i] == data[i + 1] && s_types[i + 1]);
-		}
+		// Initialize the suffix array to 'unknowns'
+		std::fill(SA.begin(), SA.end(), -1);
 
-		// Stage 1: reduce the problem by at least 1/2. Sort all the S-substrings
+		// Stage 1: reduce the problem by at least 1/2. Sort all the S-suffixes
 		{
 			auto bkt = std::vector<int>(alphabet_size + 1);
 
-			// Find ends of buckets
-			GetBuckets(data, bkt, true);
-			std::fill(SA.begin(), SA.end(), -1);
+			// Find the indices for the end of each character bucket in 'SA'
+			GetBuckets<Int, false>(data, bkt);
+
+			// Record the index of each "LMS" suffix in the bucket corresponding to its first character.
+			// Fill the buckets from the right (that's why bucket end indices are used)
 			for (int i = 1; i != std::ssize(data); ++i)
 			{
-				if (IsLeftmostSType(i, s_types))
+				if (sfx_type.IsLeftmostSType(i))
 					SA[--bkt[data[i]]] = i;
 			}
-			InduceSuffixArrayL(s_types, SA, data, bkt, false);
-			InduceSuffixArrayS(s_types, SA, data, bkt, true);
+
+			InduceSortLtoR(sfx_type, SA, data, bkt);
+			InduceSortRtoL(sfx_type, SA, data, bkt);
 		}
 
-		// Compact all the sorted substrings into the first 'n1' items of SA.
-		// 2*n1 is not larger than 'size' (proveable)
-		int n1 = 0;
+		// Compact all the sorted LMS suffixes into the first 'n1' items of SA.
+		// '2*n1' is not larger than 'size' (proveable)
+		ptrdiff_t n1 = 0;
 		for (int i = 0; i != std::ssize(data); ++i)
-			if (IsLeftmostSType(SA[i], s_types))
+			if (sfx_type.IsLeftmostSType(SA[i]))
 				SA[n1++] = SA[i];
 
-		// Find the lexicographic names of substrings.
+		// Reset the unused space in 'SA'
 		std::fill(SA.begin() + n1, SA.end(), -1);
+
+		// Assign lexicographic names to the LMS suffixes.
 		int name = 0, prev = -1;
-		for (int i = 0; i < n1; ++i)
+		for (auto i = 0; i != n1; ++i)
 		{
-			int pos = SA[i];
-			bool diff = false;
-			for (int d = 0; d < std::ssize(data); ++d)
+			auto pos = SA[i];
+			for (int d = 0; d != std::ssize(data); ++d)
 			{
-				if (prev == -1 || data[pos + d] != data[prev + d] || s_types[pos + d] != s_types[prev + d])
+				// If this substring is different from the previous one, then it gets a new name.
+				if (prev == -1 || data[pos + d] != data[prev + d] || sfx_type.IsSType(pos + d) != sfx_type.IsSType(prev + d))
 				{
-					diff = true;
+					name++;
+					prev = pos;
 					break;
 				}
-				if (d > 0 && (IsLeftmostSType(pos + d, s_types) || IsLeftmostSType(prev + d, s_types)))
+				if (d > 0 && (sfx_type.IsLeftmostSType(pos + d) || sfx_type.IsLeftmostSType(prev + d)))
 				{
 					break;
 				}
 			}
-			if (diff)
-			{
-				name++;
-				prev = pos;
-			}
+
 			pos = (pos % 2 == 0) ? pos / 2 : (pos - 1) / 2;
 			SA[n1 + pos] = name - 1;
 		}
 
-		for (int i = (int)std::ssize(data) - 1, j = (int)std::ssize(data) - 1; i >= n1; --i)
+		for (auto i = std::ssize(data) - 1, j = i; i >= n1; --i)
 		{
 			if (SA[i] >= 0)
 				SA[j--] = SA[i];
 		}
 
 		// Stage 2: solve the reduced problem.
-		// Recurse if names are not yet unique.
+		auto data1 = SA.subspan(data.size() - n1, n1);
 		auto SA1 = SA.subspan(0, n1);
-		auto s1 = SA.subspan(std::ssize(data) - n1, n1);
+
+		// Recurse if names are not yet unique.
 		if (name < n1)
 		{
-			Build<int>(s1, SA1, name - 1);
+			Build<int>(data1, SA1, name - 1);
 		}
 		else
 		{
-			// Generate the suffix array of s1 directly
+			// Generate the suffix array of 'data1' directly
 			for (int i = 0; i != n1; ++i)
-				SA1[s1[i]] = i;
+				SA1[data1[i]] = i;
 		}
 
 		// Stage 3: induce the result for the original problem.
 		{
 			auto bkt = std::vector<int>(alphabet_size + 1);
 
-			// put all the LMS characters into their buckets
-			// find ends of buckets
-			GetBuckets(data, bkt, true);
+			// Find ends of buckets
+			GetBuckets<Int, false>(data, bkt);
+
+			// Put all the LMS characters into their buckets
 			for (int i = 1, j = 0; i < std::ssize(data); ++i)
-				if (IsLeftmostSType(i, s_types))
-					s1[j++] = i; // get p1
+				if (sfx_type.IsLeftmostSType(i))
+					data1[j++] = i; // get p1
 
 			// Get index in src
 			for (int i = 0; i < n1; i++)
-				SA1[i] = s1[SA1[i]];
+				SA1[i] = data1[SA1[i]];
+
+			std::fill(SA.begin() + n1, SA.end(), -1);
 
 			// Init SA[n1..n-1]
-			std::fill(SA.begin() + n1, SA.end(), -1);
-			for (int i = n1 - 1; i >= 0; i--)
+			for (auto i = n1 - 1; i >= 0; i--)
 			{
 				auto j = SA[i];
 				SA[i] = -1;
 				SA[--bkt[data[j]]] = j;
 			}
 
-			InduceSuffixArrayL(s_types, SA, data, bkt, false);
-			InduceSuffixArrayS(s_types, SA, data, bkt, true);
+			InduceSortLtoR(sfx_type, SA, data, bkt);
+			InduceSortRtoL(sfx_type, SA, data, bkt);
 		}
 	}
 
@@ -309,12 +381,11 @@ namespace pr::suffix_array
 		return 0; //todo
 	}
 
-
 	// Return the locations of the occurrances of a substring 
 	template <typename T, std::integral Int>
 	std::vector<size_t> Find(const T* data, size_t size, Int* sa, size_t sa_size, const T* sub, size_t sub_size)
 	{
-		return {};
+		return {}; //todo
 	}
 }
 
@@ -351,8 +422,28 @@ namespace pr::container
 			PR_CHECK(suffix_array::Contains<char>({ "rab", 3 }, data, sa), false);
 
 			//PR_CHECK(suffix_array::Count<char>({ "ab", 2 }, data, sa) == 2, true);
-
 		}
+		{
+			std::uniform_int_distribution<int> dist('a', 'z');
+			std::default_random_engine rng(0);
+
+			std::string data(1024, '\0');
+			for (auto& c : data)
+				c = static_cast<char>(dist(rng));
+			data.push_back(0);
+
+			std::vector<int> sa(data.size());
+			suffix_array::Build<char>(data, sa, 256);
+
+			// Check that each substring is less than the next
+			for (size_t i = 0; i < data.size() - 1; ++i)
+			{
+				auto a = std::string_view(data.data() + sa[i]);
+				auto b = std::string_view(data.data() + sa[i + 1]);
+				PR_CHECK(a < b, true);
+			}
+		}
+
 		//{// Binary data
 		//	//                    0    1    2    3    4    5    6    7    8    9    a    b
 		//	int const data[] = { 'a', 'b', 'r', 'a', 'c', 'a', 'd', 'a', 'b', 'r', 'a' };
