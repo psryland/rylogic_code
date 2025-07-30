1751ec00e49feb65c45483acb1b6e4ddc46adc6c
Paul Ryland <psryland@gmail.com>
2025-07-30 23:47:27 +1200

    Test CPP project

--- projects/tests/test_cpp/src/main.cpp
+++ projects/tests/test_cpp/src/main.cpp
@@ -1,91 +1,26 @@
-
-#define GUIAPP 0 // remember to change linker->system->subsystem
-#define D3D_DEBUG_INFO
-
-#if GUIAPP
-#   define MainFunc() APIENTRY tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
-#else
-#   define _CONSOLE
-#   define MainFunc() main(int argc, char* argv[])
-#endif
-
-#define PR_MATHS_USE_D3DX 1
-#define PR_MATHS_USE_INTRINSICS 1
-
+﻿#include <iostream>
 #include "pr/common/min_max_fix.h"
-//#include "pr/common/atl.h"
-//#include <windows.h>
-//#include <stdio.h>
-#include <conio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <string>
-//#include <sstream>
-//#include <fstream>
-//#include <d3d9.h>
-//#include <d3dx9.h>
-//#include <vector>
-//#include "pr/common/assert.h"
-//#include "pr/common/array.h"
-//#include "pr/common/fmt.h"
-//#include "pr/common/timers.h"
-//#include "pr/common/imposter.h"
-//#include "pr/common/proxy.h"
-//#include "pr/maths/maths.h"
-//#include "pr/maths/stat.h"
-//#include "pr/filesys/fileex.h"
-//#include "pr/gui/misc.h"
-//#include "pr/gui/progress_dlg.h"
-//#include "pr/geometry/geometry.h"
-//#include "pr/gui/grid_ctrl.h"
-//#include "pr/gui/gdiplus.h"
-//#include "pr/gui/context_menu.h"
-//#include "pr/gui/graph_ctrl.h"
-#include "pr/hardware/comm_port_io.h"
-//#include "pr/threads/background_task.h"
-//#include "pr/linedrawer/ldr_helper.h"
-//#include "pr/input/dinput.h"
 
-#pragma warning (disable:4100) // unused params
-int MainFunc()
+namespace tests
 {
-	pr::CommPortIO io;
-	
-	std::string data = "Hello Comm Port";
-	io.Open(3);
-	io.Write(&data[0], data.size(), INFINITE);
-	for (;!_kbhit();)
+	// Enable a test file
+	void Run();
+}
+
+#if 0
+int APIENTRY tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow) { (void)hInstance, hPrevInstance, lpCmdLine, nCmdShow;
+#else
+int main(int argc, char* argv[]) { (void)argc, argv;
+#endif
+	try
 	{
-		size_t size = io.BytesAvailable();
-		printf("bytes available: %d\n", size);
+		tests::Run();
+		return 0;
 	}
-
-	//using namespace pr::gui;
-	//pr::GdiPlus gdiplus;
-	
-	//AllocConsole();
-	//HWND hwnd = GetConsoleWindow();
-	
-	//pr::gui::CGraphCtrl<> graph;
-	
-	//StylePtr st0(new ContextMenuStyle());
-	//st0->m_col_bkgd.SetValue(Gdiplus::ARGB(Gdiplus::Color::Black));
-	//st0->m_col_text.SetValue(Gdiplus::ARGB(Gdiplus::Color::Red));
-
-	//ContextMenu menu(L"Menu", 10, StylePtr(new ContextMenuStyle()));
-	//menu.AddItem(new ContextMenu::Label(L"Pauls 0", 11, StylePtr(), 1));
-	//menu.AddItem(new ContextMenu::Label(L"Pauls 1", 12, st0, 2));
-	//ContextMenu& sub = menu.AddItem(new ContextMenu(L"Sub Menu", 100));
-	//sub.AddItem(new ContextMenu::Label(L"Rules 0", 101));
-	//sub.AddItem(new ContextMenu::Label(L"Rules 1", 102));
-
-	//int res = menu.Show(hwnd, 100, 100);
-	//
-	//switch (res)
-	//{
-	//case 11: printf("Pauls 0\n"); break;
-	//case 12: printf("Pauls 1\n"); break;
-	//}
-	return 0;
+	catch (std::exception const& ex)
+	{
+		std::cerr << ex.what() << "\n";
+		return -1;
+	}
 }
 
---
+++ projects/tests/test_cpp/src/tests/test_ldraw.cpp
@@ -0,0 +1,18 @@
+﻿#if 1
+#include "pr/common/min_max_fix.h"
+#include "pr/network/socket_stream.h"
+#include "pr/view3d-12/ldraw/ldraw_builder.h"
+
+namespace tests
+{
+	void Run()
+	{
+		pr::network::Winsock winsock;
+		pr::network::socket_stream ldr("localhost", 1976);
+
+		pr::rdr12::ldraw::Builder builder;
+		builder.Group("g", 0xFFFF0000).Box("b", 0xFF00FF00).dim(1, 2, 3);
+		std::cout << builder.ToString(true);
+	}
+}
+#endif
